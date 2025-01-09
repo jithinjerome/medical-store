4a6c6d77f95b6b16dfa8ffdb260b2b558b2cf831
jithinjerome <jithinsjerome@gmail.com>
2025-01-09 13:24:29 +0530

    Added methods for verifying Delivery Persons and Medical Stores.

--- src/main/java/com/example/medical/store/Admin/AdminController.java
+++ src/main/java/com/example/medical/store/Admin/AdminController.java
@@ -1,6 +1,8 @@
 package com.example.medical.store.Admin;
 
 
+import com.example.medical.store.DeliveryPerson.DeliveryPersonModel;
+import com.example.medical.store.MedicalStore.MedicalStoreModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +25,27 @@ public class AdminController {
             return new ResponseEntity<>(loginResponse, HttpStatus.OK);
         } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+    @PutMapping(path = "/verify/{id}")
+    public ResponseEntity<?> verifyPerson(@PathVariable int id){
+        try{
+            DeliveryPersonModel verifiedPerson = adminService.verifiedPerson(id);
+            return new ResponseEntity<>(verifiedPerson,HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @PutMapping(path = "/verifyStore/{id}")
+    public ResponseEntity<?> verifyStore(@PathVariable int id){
+        try{
+            MedicalStoreModel verifiedStore = adminService.verifiedStore(id);
+            return new ResponseEntity<>(verifiedStore, HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
         }
     }
 }
--- src/main/java/com/example/medical/store/Admin/AdminService.java
+++ src/main/java/com/example/medical/store/Admin/AdminService.java
@@ -1,18 +1,32 @@
 package com.example.medical.store.Admin;
 
 
+import com.example.medical.store.DeliveryPerson.DeliveryPersonModel;
+import com.example.medical.store.DeliveryPerson.DeliveryPersonRepo;
 import com.example.medical.store.JWT.JWTUtil;
+import com.example.medical.store.MedicalStore.MedicalStoreModel;
+import com.example.medical.store.MedicalStore.MedicalStoreRepo;
+import com.example.medical.store.User.VerificationStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Service
 public class AdminService {
     @Autowired
     private AdminRepo adminRepo;
+
+    @Autowired
+    private DeliveryPersonRepo deliveryPersonRepo;
+
+    @Autowired
+    private MedicalStoreRepo medicalStoreRepo;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -34,5 +48,27 @@ public class AdminService {
         }
         throw new IllegalArgumentException("Invalid Credentials: User not found");
 
+    }
+
+    public DeliveryPersonModel verifiedPerson(int id){
+        Optional<DeliveryPersonModel> deliveryPerson = deliveryPersonRepo.findById(id);
+        if(deliveryPerson.isPresent()){
+            DeliveryPersonModel persons = deliveryPerson.get();
+            persons.setVerificationStatus(VerificationStatus.VERIFIED);
+            return deliveryPersonRepo.save(persons);
+        }else {
+            throw new IllegalArgumentException("No person found with this ID");
+        }
+    }
+
+    public MedicalStoreModel verifiedStore(int id) {
+        Optional<MedicalStoreModel> medicalStore = medicalStoreRepo.findById(id);
+        if(medicalStore.isPresent()){
+            MedicalStoreModel verifiedStore = medicalStore.get();
+            verifiedStore.setVerificationStatus(VerificationStatus.VERIFIED);
+            return medicalStoreRepo.save(verifiedStore);
+        }else{
+            throw new IllegalArgumentException("No store found with this ID");
+        }
     }
 }
