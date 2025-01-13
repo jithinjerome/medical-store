2cf21a824d5366e0666c789ff04ee95e19730144
siva1210connor <siva1210connor@gmail.com>
2025-01-13 13:22:18 +0530

    updated admin controller to provide data to frontend

--- src/main/java/com/example/medical/store/Admin/AdminController.java
+++ src/main/java/com/example/medical/store/Admin/AdminController.java
@@ -1,6 +1,8 @@
 package com.example.medical.store.Admin;
 
 
+import com.example.medical.store.DeliveryPerson.DeliveryPersonModel;
+import com.example.medical.store.MedicalStore.MedicalStoreModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
