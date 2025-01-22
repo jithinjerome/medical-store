d7bb693ee92d27383b261e90553d3cc2a73fbf2d
siva1210connor <siva1210connor@gmail.com>
2025-01-22 13:05:36 +0530

    Updates from master

--- src/main/java/com/example/medical/store/JWT/SecurityConfig.java
+++ src/main/java/com/example/medical/store/JWT/SecurityConfig.java
@@ -40,7 +40,16 @@ public class SecurityConfig {
                                 "/api/auth/admin/login",
                                 "/api/auth/admin/users",
                                 "/api/auth/medical-store/register",
-                                "/api/auth/medical-store/login"
+                                "/api/auth/medical-store/login",
+                                "/api/auth/admin/allStores",
+                                "/api/auth/admin/delivery-persons",
+                                "/api/auth/admin/verifyStore/{id}",
+                                "/api/auth/admin/revokeVerifyStore/{storeId}",
+                                "/api/auth/admin/verifyDeliveryPerson/{id}",
+                                "/api/auth/admin/revokeDeliveryPerson/{id}",
+                                "/api/auth/admin/removeStore/{id}",
+                                "/api/auth/admin/removeDeliveryPerson/{id}",
+                                "/api/auth/admin//uploadLicense/{storeId}"
 
                         ).permitAll()
                         .requestMatchers(
