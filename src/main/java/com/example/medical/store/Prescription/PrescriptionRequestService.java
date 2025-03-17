57482f0bdd3aa8cea5079373bf338eb32b29f974
siva1210connor <siva1210connor@gmail.com>
2025-03-17 14:33:06 +0530

    updated prescription request service

--- src/main/java/com/example/medical/store/Prescription/PrescriptionRequestService.java
+++ src/main/java/com/example/medical/store/Prescription/PrescriptionRequestService.java
@@ -2,7 +2,6 @@ package com.example.medical.store.Prescription;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
